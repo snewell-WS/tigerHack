d87f633d593916e6009723a10fba1346eb0271af
Sean Newell <snewell234@gmail.com>
2021-11-06 16:00:59 -0500

    Update moon_phases.py
    Made Functions for use in Determining Current Lunar Phase

--- moon_phases.py
+++ moon_phases.py
@@ -1,30 +1,43 @@
 from skyfield.api import load
 from skyfield.framelib import ecliptic_frame
+#For use in Determining MoonPhase
+def MoonPhase():
+    #using skyfield
+    ts = load.timescale()
+    t = ts.now()
+    eph = load('de421.bsp')
+    sun, moon, earth = eph['sun'], eph['moon'], eph['earth']
 
-ts = load.timescale()
-t = ts.now()
-eph = load('de421.bsp')
-sun, moon, earth = eph['sun'], eph['moon'], eph['earth']
+    e = earth.at(t)
+    _, slon, _ = e.observe(sun).apparent().frame_latlon(ecliptic_frame)
+    _, mlon, _ = e.observe(moon).apparent().frame_latlon(ecliptic_frame)
+    phase = (mlon.degrees - slon.degrees) % 360.0
+    return phase
 
-e = earth.at(t)
-_, slon, _ = e.observe(sun).apparent().frame_latlon(ecliptic_frame)
-_, mlon, _ = e.observe(moon).apparent().frame_latlon(ecliptic_frame)
-phase = (mlon.degrees - slon.degrees) % 360.0
 
-print('{0:.1f}'.format(phase))
-if(phase==0):
-    print("Its a new moon")
-if(phase>0 and phase<90):
-    print("The moon is a waxing cresent")
-if(phase==90):
-    print("its a first quarter moon")
-if(phase>90 and phase<180):
-    print("the moon is a waxing gibbus")
-if(phase==180):
-    print("the moon is full")
-if(phase>180 and phase<270):
-    print("the moon is a waning gibbus")
-if(phase==270):
-    print("its a third quarter moon")
-if(phase>270):
-    print("the moon is a waning cressent")
+def determinePhase(phase):
+    phaseOfMoon = "none"
+    if(phase==0):
+        phaseOfMoon = "NewMoon"
+        return phaseOfMoon
+    if(phase>0 and phase<90):                 
+        phaseOfMoon = "WaxingCrescent"
+        return phaseOfMoon
+    if(phase==90):        
+        phaseOfMoon = "FirstQuarter"
+        return phaseOfMoon
+    if(phase>90 and phase<180):       
+        phaseOfMoon = "WaxingGibbus"
+        return phaseOfMoon
+    if(phase==180):        
+        phaseOfMoon = "FullMoon"
+        return phaseOfMoon
+    if(phase>180 and phase<270):        
+        phaseOfMoon = "WaningGibbus"
+        return phaseOfMoon
+    if(phase==270):      
+        phaseOfMoon = "ThirdQuarter"
+        return phaseOfMoon
+    if(phase>270):        
+        phaseOfMoon = "WaningCrescent"
+        return phaseOfMoon
